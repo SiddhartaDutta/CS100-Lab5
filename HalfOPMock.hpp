eec0e6beb2848916c180b69fc28af9d9665f96e2
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-05-12 22:02:54 -0700

    Added half and negative opmocks

---
+++ HalfOPMock.hpp
@@ -0,0 +1,15 @@
+#ifndef __HALFOPMOCK_HPP__
+#define __HALFOPMOCK_HPP__
+
+#include "base.hpp"
+
+class HalfOPMock : public Base{
+
+	public:
+		HalfOPMock() : Base(){}
+		virtual double evaluate(){ return 0.5; }
+		virtual std::string stringify(){ return "0.5"; }
+
+}
+
+#endif
---
+++ NegativeOPMock.hpp
@@ -0,0 +1,15 @@
+#ifndef __NEGATIVEOPMOCK_HPP__
+#define __NEGATIVEOPMOCK_HPP__
+
+#include "base.hpp"
+
+class NegativeOPMock : public Base {
+
+	public:
+		NegativeOPMock() : Base(){}
+		virtual double evaluate(){ return -987.325; }
+		virtual std::string stringify(){ return "-987.325"; }
+
+}
+
+#endif

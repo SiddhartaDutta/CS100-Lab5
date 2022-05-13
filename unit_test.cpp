2fedfb51c7a04eb8f306eda236ab5820ab8e739f
sarahkimmieee <itssarahkim@gmail.com>
2022-05-12 23:36:23 -0700

    added tests

--- unit_test.cpp
+++ unit_test.cpp
@@ -8,6 +8,8 @@
 #include "op_test.hpp"
 
 #include "add.hpp"
+#include "sub.hpp"
+#include "pow.hpp"
 
 TEST(AddTest,AddNegNums){
        Base* element1 = new NegativeOPMock();
@@ -33,6 +35,55 @@ TEST(AddTest,AddWithZero){
        EXPECT_EQ(result,0.5);
 
 }
+TEST(SubTest,SubSameNegNums){
+       Base* element1 = new NegativeOPMock();
+       Base* element2 = new NegativeOPMock();
+       Base* subtract = new Sub(element1,element2);
+       double result = subtract->evaluate();
+       EXPECT_EQ(result,0);
+}
+
+TEST(SubTest,SubPosNums){
+       Base* element1 = new NineOPMock();
+       Base* element2 = new HalfOPMock();
+       Base* subtract = new Sub(element1,element2);
+       double result = subtract->evaluate();
+       EXPECT_EQ(result,8.5);
+}
+
+TEST(SubTest,SubWithZero){
+       Base* element1 = new ZeroOPMock();
+       Base* element2 = new HalfOPMock();
+       Base* subtract = new Sub(element1,element2);
+       double result = subtract->evaluate();
+       EXPECT_EQ(result,-0.5);
+
+}
+TEST(PowTest,RaisedBySameNum){
+       Base* bas = new NineOPMock();
+       Base* power = new NineOPMock();
+       Base* exponent = new Pow(bas,power);
+       double result = exponent->evaluate();
+       EXPECT_EQ(result,387420489);
+}
+
+TEST(PowTest,RaisedByDifNum){
+       Base* bas = new NineOPMock();
+       Base* power = new HalfOPMock();
+       Base* exponent = new Pow(bas,power);
+       double result = exponent->evaluate();
+       EXPECT_EQ(result,3);
+}
+
+TEST(PowTest,RaisedByZero){
+       Base* bas = new HalfOPMock();
+       Base* power = new ZeroOPMock();
+       Base* exponent = new Pow(bas,power);
+       double result = exponent->evaluate();
+       EXPECT_EQ(result,1);
+
+}
+
 
 
 int main(int argc, char **argv) {
